bb20f58dd32e69659cc06bb359ed3286281dc708
iambenzaid <iambenzaid@proton.me>
2023-09-08 03:42:39 +0100

    Task 101, Commit 5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - printing a short message
- * Return: return 1
- */
+*main - printing a short quote
+*
+*Return: return 1
+*/
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
