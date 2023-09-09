66a6d258886e322213b8c4e36875efca2e4be2e7
iambenzaid <iambenzaid@proton.me>
2023-09-09 02:51:07 +0100

    Task 2, Commit 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Print lowercase
+*
+*Return: return 0
+*/
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+
+}
