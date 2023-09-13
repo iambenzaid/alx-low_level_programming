38a596e915dc51588adc9fa9ca877f2047c5399d
iambenzaid <iambenzaid@proton.me>
2023-09-13 04:05:49 +0100

    Task 100, Commit 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* print_times_table - prints Table
+* @n: number line and column
+**/
+void print_times_table(int n)
+{
+	int i,j;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int value = i * j;
+
+			if (value < 10)
+			{
+				_putchar(value + '0');
+			}
+			else
+			{
+				_putchar((value / 10) + '0');
+				_putchar((value % 10) + '0');
+			}
+
+			if (j < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
