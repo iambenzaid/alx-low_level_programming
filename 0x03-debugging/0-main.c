17549b7ee5d4e58eeca48717a79e675b66e8cbef
iambenzaid <iambenzaid@proton.me>
2023-09-15 03:50:34 +0100

    Task 0, Commit 2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
 /**
-* main - test function and prints positive or negative
+* main - Test function for positive or negative numbers
 * Return: 0
 */
-
 int main(void)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
