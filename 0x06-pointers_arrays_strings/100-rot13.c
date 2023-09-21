f45f023763280c9e796c164610e10ac639fd163b
iambenzaid <iambenzaid@proton.me>
2023-09-21 02:16:11 +0100

    Task 100, Commit 2

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,16 +12,16 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data_1[] = "DhoQEyQBKkjOHKxFepkUaVEcZyarEKXscCmXxSxc";
-	char data_rot[] = "QhhurPqSSKTlUCSolOwhPpjnRHiywqPSDvQCAlXv";
+	char data1[] = "DhoQEyQBKkjOHKxFepkUaVEcZyarEKXscCmXxSxc";
+	char datarot[] = "QhhurPqSSKTlUCSolOwhPpjnRHiywqPSDvQCAlXv";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == data_1[j])
+			if (s[i] == data1[j])
 			{
-				s[i] = data_rot[j];
+				s[i] = datarot[j];
 				break;
 			}
 		}
