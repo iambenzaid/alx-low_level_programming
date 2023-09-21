51adcd95bfe36158f3a5fb0c72a67244c7814d7f
iambenzaid <iambenzaid@proton.me>
2023-09-21 02:51:28 +0100

    Task 100, Commit 4

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,29 +2,27 @@
 #include <stdio.h>
 
 /**
- * rot13 - main function
- *
- * @s: String poiner
- *
- * Return: String function
+ * rot13 - function encoder
+ * @s: pointer to string
+ * Return: return s
  */
 
 char *rot13(char *s)
 {
-	int i, j;
+	int i;
+	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	    for (j = 0; j < 52; j++)
-	    {
-	        if (s[i] == data1[j])
-	        {
-	            s[i] = datarot[j];
-	            break;
-	        }
-	    }
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
 	}
 	return (s);
 }
