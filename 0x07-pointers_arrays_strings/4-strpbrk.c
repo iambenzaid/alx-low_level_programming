196c6c8bab881665e25dbbb8759792498942126e
iambenzaid <iambenzaid@proton.me>
2023-09-26 00:14:35 +0100

    Task 4, Commit 1

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strpbrk - searche a string for any of a set of bytes
+* @s: string
+* @accept: string
+*
+* Return: string
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (*(s + i))
+	{
+		for (j = 0; *(accept + j); j++)
+			if (accept[j] == *(s + i))
+				return (s + i);
+		i++;
+	}
+	return ('\0');
+}
