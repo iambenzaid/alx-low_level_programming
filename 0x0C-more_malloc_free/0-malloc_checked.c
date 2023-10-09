8bb1bd7a68a70da78ed91ef66e2ce15bb6227c87
iambenzaid <iambenzaid@proton.me>
2023-10-09 02:20:59 +0100

    Task 0, Commit 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+* malloc_checked - function that allocates memory using malloc
+* @b: Int
+*
+* Return: void
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+*README File* for 0x0C-more_malloc_free
