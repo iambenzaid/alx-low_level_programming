beda69b0fb2c7c749398fa028dccb5124c961f46
iambenzaid <iambenzaid@proton.me>
2023-10-12 02:38:19 +0100

    Fix advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,30 +11,33 @@
 
 int main(int argc, char *argv[])
 {
-	int (*f)(int, char **) = main;
-	int i = 0, nmb;
-	unsigned char x;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	nmb = atoi(argv[1]);
-	if (nmb < 0)
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	while (i < nmb)
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
 	{
-		x = *((unsigned char *)f + i);
-		printf("%.2x", x);
-		if (i == nmb - 1)
-			continue;
-		printf(" ");
-	i++;
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
 	}
-	printf("\n");
 	return (0);
 }
