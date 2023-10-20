c882808f9c33d9633f04ac7bae7a737fb844eccf
iambenzaid <iambenzaid@proton.me>
2023-10-20 03:24:18 +0100

    Advanced Tasks in Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* before_main - Function that print a phrase before main func
+*
+* Return: void
+*/
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
