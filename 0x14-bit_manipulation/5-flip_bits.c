6ffa55b8c4ffc0522beeb8705928ae2d3e495be6
iambenzaid <iambenzaid@proton.me>
2023-10-26 23:03:14 +0100

    Mandatory tasks of 0x14

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* flip_bits - returns the number of bits you would need to flip 
+* to get from one number to another
+* @n: first Int
+* @m: secind Int
+*
+* Return: Number of bits
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int d = n ^ m;
+	int count = 0;
+
+	while (d)
+	{
+		if (d & 1)
+			count++;
+		d = d >> 1;
+	}
+	return (count);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+*README File for 0x14-bit_manipulation task*
